2313e0c7599c4470a24998a34bc349d7e8843e69
Ploux <peter@humo.golf>
2024-05-27 17:36:16 -0500

    initial commit

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Peter Loux on 5/27/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
